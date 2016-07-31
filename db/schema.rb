51183beb508663407cc15b09b5aeb3a7b5920e9f
Jane Du <Jane.dujuan.hr@gmail.com>
2016-08-01 05:30:54 +0800

    show group's creator info

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731102511) do
+ActiveRecord::Schema.define(version: 20160731132224) do
 
   create_table "groups", force: :cascade do |t|
     t.string  "title"
     t.text    "description"
     t.integer "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
