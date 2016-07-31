19b09ccb521d2082162b653a860cf1d99c0ab164
Jane Du <Jane.dujuan.hr@gmail.com>
2016-07-31 20:27:38 +0800

    add permission check

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new,:create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups=Group.all
@@ -20,12 +21,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group=Group.find(params[:id])
 
   end
 
   def update
-    @group=Group.find(params[:id])
+    
     if @group.update(params_group)
       redirect_to groups_path
     else
@@ -34,7 +34,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group=Group.find(params[:id])
+
     @group.destroy
     redirect_to groups_path
   end
@@ -48,4 +48,17 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:title, :description)
 
   end
+
+  private
+
+ def find_group_and_check_permission
+   @group = Group.find(params[:id])
+
+   if current_user != @group.user
+     redirect_to root_path, alert: "You have no permission."
+   end
+ end
+
+
+
 end
